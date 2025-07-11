df73d2851d2a35f986ea521b94a49a07f4bb4092
Rishabh <rishabh.prajapati@rapidops.com>
2025-07-11 16:34:42 +0530

    feat: update NestJS setup with Prisma integration and streamline app module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
-import { UsersController } from './controllers/users/users.controller';
-import { UsersService } from './services/users/users.service';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
